a0ad77f3081830e20e9b683ecaa9e94e9b826a54
Swarleys <elessar29@gmail.com>
2018-05-22 20:58:59 +0200

    Search Results Pagination

--- src/js/index.js
+++ src/js/index.js
@@ -2,7 +2,6 @@ import Search from "./models/Search";
 import * as searchView from "./views/SearchView";
 import { elements, renderLoader, clearLoader } from "./views/base";
 
-
 // Global state of the app
 // Search object
 // Current recipe object
@@ -15,7 +14,7 @@ const controlSearch = async () => {
   // 1) Get query from view
   const query = searchView.getInput();
 
-  if(query) {
+  if (query) {
     // 2) New search object and add to state
     state.search = new Search(query);
 
@@ -31,10 +30,18 @@ const controlSearch = async () => {
     clearLoader();
     searchView.renderResults(state.search.result);
   }
-}
+};
 
 elements.searchForm.addEventListener("submit", e => {
   e.preventDefault();
   controlSearch();
 });
 
+elements.searchResPages.addEventListener("click", e => {
+  const btn = e.target.closest(".btn-inline");
+  if (btn) {
+    const goToPage = parseInt(btn.dataset.goto);
+    searchView.clearResults();
+    searchView.renderResults(state.search.result, goToPage);
+  }
+});
--- src/js/views/SearchView.js
+++ src/js/views/SearchView.js
@@ -1,14 +1,19 @@
 import { elements } from "./base";
 
 export const getInput = () => elements.searchInput.value;
-export const clearInput = () => { elements.searchInput.value = ""; }
-export const clearResults = () => { elements.searchResList.innerHTML = ""; }
+export const clearInput = () => {
+  elements.searchInput.value = "";
+};
+export const clearResults = () => {
+  elements.searchResList.innerHTML = "";
+  elements.searchResPages.innerHTML = "";
+};
 
 const limitRecipeTitle = (title, limit = 17) => {
   const newTitle = [];
-  if(title.length > limit) {
+  if (title.length > limit) {
     title.split(" ").reduce((acc, cur) => {
-      if(acc + cur.length <= limit) {
+      if (acc + cur.length <= limit) {
         newTitle.push(cur);
       }
       return acc + cur.length;
@@ -36,6 +41,43 @@ const renderRecipe = recipe => {
   elements.searchResList.insertAdjacentHTML("beforeend", markup);
 };
 
-export const renderResults = recipes => {
-  recipes.forEach(renderRecipe);
+const createButton = (page, type) => `
+ <button class="btn-inline results__btn--${type}" data-goto=${type === "prev" ? page - 1 : page + 1}>
+    <span>Page ${type === "prev" ? page - 1 : page + 1}</span>
+      <svg class="search__icon">
+          <use href="img/icons.svg#icon-triangle-${type === "prev" ? "left" : "right"}"></use>
+      </svg>
+  </button>
+`;
+
+const renderButtons = (page, numResults, resPerPage) => {
+  const pages = Math.ceil(numResults / resPerPage);
+  let button;
+
+  if (page === 1 && pages > 1) {
+    // Button to go to next page
+    button = createButton(page, "next");
+  } else if (page < pages) {
+    // Both buttons
+    button = `
+      ${createButton(page, "prev")}
+      ${createButton(page, "next")}
+    `
+  } else if (page === pages && pages > 1) {
+    // Only button to go to the prev page
+    button = createButton(page, "prev");
+  }
+
+  elements.searchResPages.insertAdjacentHTML("afterbegin", button);
+};
+
+export const renderResults = (recipes, page = 1, resPerPage = 10) => {
+  // render results of current page
+  const start = (page - 1) * resPerPage;
+  const end = page * resPerPage;
+
+  recipes.slice(start, end).forEach(renderRecipe);
+
+  // render pagination
+  renderButtons(page, recipes.length, resPerPage);
 };
