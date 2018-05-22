a0c88b4fe41e0df0ced7062c59622f759e5474ed
Swarleys <elessar29@gmail.com>
2018-05-22 15:56:50 +0200

    setting up webpack and the api calls

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,17 @@
-// Global app controller
+import axios from "axios";
+
+async function getResults(query) {
+  const proxy = "https://cors-anywhere.herokuapp.com/";
+  const key = "a461d9fd6fe14baa30b394fa968e60c7";
+
+  try {
+    const res = await axios(
+      `${proxy}http://food2fork.com/api/search?key=${key}&q=${query}`
+    );
+    const recipes = res.data.recipes;
+    console.log(recipes);
+  } catch (error) {
+    console.log(error);
+  }
+}
+getResults("tomato pasta");
